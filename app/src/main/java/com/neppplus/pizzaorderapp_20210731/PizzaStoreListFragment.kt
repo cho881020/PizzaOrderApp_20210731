c509ff61e4161a263a0ab2af9a9ac3bb993e6ae3
조경진 <cho881020@gmail.com>
2021-08-07 10:15:17 +0900

    기본 UI + 피자가게 프래그먼트 (임시) 추가

---
+++ app/src/main/java/com/neppplus/pizzaorderapp_20210731/PizzaStoreListFragment.kt
@@ -0,0 +1,28 @@
+package com.neppplus.pizzaorderapp_20210731
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class PizzaStoreListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_pizza_store_list, container, false)
+    }
+
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+
+
+    }
+
+
+}
