20046dd5ed523c26d1587370bc20b858d4f8b874
조경진 <cho881020@gmail.com>
2021-08-07 10:44:51 +0900

    피자 가게 어댑 뼈대작업 진행 마무리  - xml을 객체로 가져오는 작업

--- app/src/main/java/com/neppplus/pizzaorderapp_20210731/adapters/PizzaStoreAdapter.kt
+++ app/src/main/java/com/neppplus/pizzaorderapp_20210731/adapters/PizzaStoreAdapter.kt
@@ -1,7 +1,11 @@
 package com.neppplus.pizzaorderapp_20210731.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.neppplus.pizzaorderapp_20210731.R
 import com.neppplus.pizzaorderapp_20210731.datas.Store
 
 class PizzaStoreAdapter(
@@ -9,5 +13,18 @@ class PizzaStoreAdapter(
     val resId : Int,
     val mList : ArrayList<Store> ) : ArrayAdapter<Store>(mContext, resId, mList) {
 
+    val mInflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow = mInflater.inflate(R.layout.pizza_store_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        return row
+    }
 
 }
