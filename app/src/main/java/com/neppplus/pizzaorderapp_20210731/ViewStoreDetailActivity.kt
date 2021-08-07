7769d07c3aede7611e7ff4032755c553f29e9290
조경진 <cho881020@gmail.com>
2021-08-07 11:36:56 +0900

    넘겨 받은 가게 데이터 표시

--- app/src/main/java/com/neppplus/pizzaorderapp_20210731/ViewStoreDetailActivity.kt
+++ app/src/main/java/com/neppplus/pizzaorderapp_20210731/ViewStoreDetailActivity.kt
@@ -2,7 +2,9 @@ package com.neppplus.pizzaorderapp_20210731
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import com.neppplus.pizzaorderapp_20210731.datas.Store
+import kotlinx.android.synthetic.main.activity_view_store_detail.*
 
 class ViewStoreDetailActivity : AppCompatActivity() {
 
@@ -22,6 +24,11 @@ class ViewStoreDetailActivity : AppCompatActivity() {
     fun setValues() {
 
         mStore = intent.getSerializableExtra("store") as Store
+
+        nameTxt.text = mStore.name
+        phoneNumTxt.text = mStore.phoneNum
+
+        Glide.with(this).load(mStore.imageURL).into(logoImg)
 
     }
 
