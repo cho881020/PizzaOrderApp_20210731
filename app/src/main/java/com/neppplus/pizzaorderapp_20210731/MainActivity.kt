f192b422dea76a11f8867671da814214581ad5a8
조경진 <cho881020@gmail.com>
2021-08-07 10:25:39 +0900

    뷰페이저+탭레이아웃 화면 연동

--- app/src/main/java/com/neppplus/pizzaorderapp_20210731/MainActivity.kt
+++ app/src/main/java/com/neppplus/pizzaorderapp_20210731/MainActivity.kt
@@ -2,10 +2,32 @@ package com.neppplus.pizzaorderapp_20210731
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.pizzaorderapp_20210731.adapters.MainViewPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mainViewPagerAdapter: MainViewPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
     }
+
+    fun setupEvents() {
+//        onClick 등의 이벤트 처리 코드 모음.
+    }
+
+    fun setValues() {
+//        화면에 데이터 (UI) 를 띄우기 위한 관련 코드 모음.
+
+        mainViewPagerAdapter = MainViewPagerAdapter( supportFragmentManager )
+        mainViewPager.adapter = mainViewPagerAdapter
+
+        mainTabLayout.setupWithViewPager(mainViewPager)
+
+    }
+
 }
